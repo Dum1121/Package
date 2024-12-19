4447db37c78bbdf7b18e37485b1801a1df800339
Dum <91747868+Dum1121@users.noreply.github.com>
2024-12-19 15:13:47 +0700

    ?

--- Scripts/Adoptme.lua
+++ Scripts/Adoptme.lua
@@ -26,3 +26,62 @@ Stores = {
     "SimonSaysMinigame",
     "WinterCastle"
 }
+
+Eggs = {
+    "cracked_egg",
+    "regular_pet_egg",
+    "royal_egg",
+    "retired_egg",
+    "seasia_2023_egg",
+    "mythic_egg",
+    "japan_2022_egg",
+    "danger_2023_egg",
+    "desert_2024_egg",
+    "garden_2024_egg"
+}
+
+gifts = {
+    "smallgift",
+    "biggift",
+    "massivegift",
+    "legend_hat_2022_simple_chest",
+    "legend_hat_2022_regal_chest"
+}
+Obbys = {
+    "miniworld",
+    "coastal_climb",
+    "shipwreck_bay",
+    "ancient_ruins",
+    "lonelypeak",
+    "pyramid",
+    "tinyisles"
+}
+
+RetardedBullShit = {
+    "CashRegister",
+    "GoldenCashRegister"
+}
+
+Beds = {
+    "FancyPetBed",
+    "ModernPetBed",
+    "CircularPetBed",
+    "RectangularPetBed",
+    "NormalCrib",
+    "BasicCrib",
+    "RoyalCrib",
+    "PoshCrib",
+}
+
+Baths = {
+    "StylishShower",
+    "CheapPetBathtub",
+    "ModernShower",
+    "Bathtub",
+    "GingerbreadShower",
+    "HotTub"
+}
+
+Toilets = {
+    "Toilet"
+}
