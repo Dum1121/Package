a18b971651a4e33a1764b8e466d5676053c53d43
Dum <91747868+Dum1121@users.noreply.github.com>
2025-02-02 17:47:10 +0700

    ?

---
+++ Scripts/Variable.lua
@@ -0,0 +1,28 @@
+UserInputService = game:GetService("UserInputService")
+RunService = game:GetService("RunService")
+TweenService = game:GetService("TweenService")
+HttpService = game:GetService("HttpService")
+TeleportService = game:GetService("TeleportService")
+CoreGui = game:GetService("CoreGui")
+Debris = game:GetService("Debris")
+VirtualUser = game:GetService("VirtualUser")
+VirtualInputManager = game:GetService("VirtualInputManager")
+ReplicatedStorage = game:GetService("ReplicatedStorage")
+Lighting = game:GetService("Lighting")
+CollectionService = game:GetService("CollectionService")
+StatsService = game:GetService("Stats")
+StarterGui = game:GetService("StarterGui")
+ScriptContext = game:GetService("ScriptContext")
+GuiService = game:GetService("GuiService")
+PathfindingService = game:GetService("PathfindingService")
+Camera = workspace.CurrentCamera
+
+ContentProvider = game:GetService("ContentProvider")
+Players = game:GetService("Players")
+plr = Players.LocalPlayer
+plrg = plr.PlayerGui
+plrs = plr.PlayerScripts
+Mouse = plr:GetMouse()
+mouse = Mouse
+
+PlaceId,JobId,GameId = game.PlaceId,game.JobId,game.GameId
